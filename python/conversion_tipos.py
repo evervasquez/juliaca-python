ca667cc43e7aa8ab7b4809b10a1c71f7951cdd40
eveR Vásquez <pever@unsm.edu.pe>
2017-09-16 10:36:19 -0500

    add conversion de tipos

--- python/conversion_tipos.py
+++ python/conversion_tipos.py
@@ -1,1 +1,15 @@
-conversion_tipos.py
+precio = "45"
+
+if type(precio) == str:
+	# convertir a float
+	precio = float(precio)
+
+precio_con_impuesto = precio * 1.19
+
+cantidad = "3"
+
+if type(cantidad) == str:
+	# convertir a entero
+	cantidad = int(cantidad)
+
+print("total de la venta: %f " % (precio_con_impuesto * cantidad))
