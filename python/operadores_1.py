0be9679bc6397021643975a251f4a44fb97ae733
eveR Vásquez <pever@unsm.edu.pe>
2017-09-16 10:08:59 -0500

    add operadores_1

---
+++ python/operadores_1.py
@@ -0,0 +1,17 @@
+numero1 = 5
+numero2 = 2
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+divicion = numero1 / numero2
+
+divicion_entera = numero1 // numero2
+modulo = numero1 % numero2
+
+print(suma)
+print(resta)
+print(multiplicacion)
+print(divicion)
+print(divicion_entera)
+print(modulo)
